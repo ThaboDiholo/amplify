d81bb6fbf5bf3fa48b49ce8198b8b4db7dfc9dd6
Thabo Diholo <TDiholo@firstdistribution.co.za>
2024-05-15 12:17:36 +0200

    Hello world Thabo Diholo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hello world Thabo
+          Edit <code>src/App.js</code> Hello world Thabo Diholo
         </p>
         <a
           className="App-link"
